0f0e77b209749c9d637ab2c1e1b3ecf3219608ca
kentavrex <khanov.a@selectel.ru>
2024-05-24 12:20:14 +0300

    update

--- disable_not_null.sql
+++ disable_not_null.sql
@@ -2,23 +2,16 @@ DO $$
 DECLARE
     schema_name TEXT := 's100000';
     not_null_count INTEGER := 0;
+    r RECORD;
 BEGIN
-    -- Отключение всех ограничений NOT NULL
-    EXECUTE format('
-        DO $$
-        DECLARE
-            r RECORD;
-        BEGIN
-            FOR r IN
-                SELECT table_name, column_name
-                FROM information_schema.columns
-                WHERE table_schema = %L AND is_nullable = ''NO''
-            LOOP
-                EXECUTE format(''ALTER TABLE %I.%I ALTER COLUMN %I DROP NOT NULL'', schema_name, r.table_name, r.column_name);
-                not_null_count := not_null_count + 1;
-            END LOOP;
-        END $$;
-    ', schema_name);
+    FOR r IN
+        SELECT table_name, column_name
+        FROM information_schema.columns
+        WHERE table_schema = schema_name AND is_nullable = 'NO'
+    LOOP
+        EXECUTE format('ALTER TABLE %I.%I ALTER COLUMN %I DROP NOT NULL', schema_name, r.table_name, r.column_name);
+        not_null_count := not_null_count + 1;
+    END LOOP;
 
     -- Вывод информации о количестве отключённых ограничений
     RAISE NOTICE 'Схема: %, Ограничений целостности типа NOT NULL отключено: %', schema_name, not_null_count;
