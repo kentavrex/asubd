b321d9d546c3fe112baf30bc13e5b4deaa68bfa6
kentavrex <khanov.a@selectel.ru>
2024-05-24 12:36:11 +0300

    update

--- disable_not_null.sql
+++ disable_not_null.sql
@@ -1,6 +1,6 @@
 DO $$
 DECLARE
-    schema_name TEXT := 's335100';
+    schema_name TEXT := 's335100'; -- измените на нужную схему
     not_null_count INTEGER := 0;
     r RECORD;
 BEGIN
@@ -9,8 +9,13 @@ BEGIN
         FROM information_schema.columns
         WHERE table_schema = schema_name AND is_nullable = 'NO'
     LOOP
-        EXECUTE format('ALTER TABLE %I.%I ALTER COLUMN %I DROP NOT NULL', schema_name, r.table_name, r.column_name);
-        not_null_count := not_null_count + 1;
+        BEGIN
+            EXECUTE format('ALTER TABLE %I.%I ALTER COLUMN %I DROP NOT NULL', schema_name, r.table_name, r.column_name);
+            not_null_count := not_null_count + 1;
+        EXCEPTION WHEN others THEN
+            -- Игнорировать ошибки при попытке удалить NOT NULL из столбцов, входящих в первичные ключи
+            CONTINUE;
+        END;
     END LOOP;
 
     -- Вывод информации о количестве отключённых ограничений
